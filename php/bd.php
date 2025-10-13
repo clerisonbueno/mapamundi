37ca0f3a4c055ecc8927d491025b49fdb4d6b9a6
Ragnaroko12 <luisgustsvo5@gmail.com>
2025-10-13 09:35:47 -0300

    segundo commit

--- php/bd.php
+++ php/bd.php
@@ -1,7 +1,5 @@
-
-
-
 <?php
+  session_start();
 function CadastrarBanco() {
     
     $pdo = new PDO('mysql:host=localhost;dbname=mapamundi','root','');
@@ -18,5 +16,27 @@ function CadastrarBanco() {
     ) 
     );
 }
+function login(){
+    $pdo = new PDO('mysql:host=localhost;dbname=mapamundi','root','');
+
+    $sql = $pdo -> prepare("SELECT * FROM `cadcli` WHERE email=? AND senha=?");
+
+    $sql->execute(array($_POST['email'], sha1($_POST['senha'])));
+
+    $dados = $sql ->fetchALL(PDO::FETCH_ASSOC);
+    print_r($dados);
+  
+    if (!empty($dados)) {
+        $usuario = $dados[0];
+        $_SESSION['nome'] = $usuario['nome'];
+        header("Location: ../index.html");
+    } else {
+        header("Location: login.php");
+
+    }
+
+}
+
+
 CadastrarBanco()
 ?>
