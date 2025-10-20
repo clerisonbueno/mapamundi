039752d7c8e5da95172f7af38974600526ef9b55
LUIS GUSTAVO DA SILVA DERONZI <luis.gsderonzi@senacsp.edu.br>
2025-10-20 10:47:58 -0300

    sql mais recente

--- mapamundi.sql
+++ mapamundi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 15/10/2025 às 20:05
+-- Tempo de geração: 20/10/2025 às 15:47
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -61,9 +61,16 @@ CREATE TABLE `cadpais` (
 --
 
 INSERT INTO `cadpais` (`id`, `pais`, `continente`, `regiao_continente`, `evento`) VALUES
-(3, 'AF', 'América', 'América Central', 'AA'),
-(4, 'DZ', 'América', 'Europa Ocidental', 'macacos'),
-(5, 'DZ', 'América', 'Europa Ocidental', 'macacos');
+(6, 'Brasil', 'América do Sul', 'América Latina', 'O Brasil é o único país da América do Sul que fala português.'),
+(7, 'Canadá', 'América do Norte', 'América Anglo-Saxônica', 'O Canadá tem mais lagos do que todos os outros países juntos.'),
+(8, 'Egito', 'África', 'Norte da África', 'O Egito abriga a mais antiga das Sete Maravilhas do Mundo Antigo — as Pirâmides de Gizé.'),
+(9, 'Japão', 'Ásia', 'Extremo Oriente', 'No Japão, existem máquinas de venda automática para praticamente tudo.'),
+(10, 'Austrália', 'Oceania', 'Australásia', 'A Austrália tem mais cangurus do que habitantes.'),
+(11, 'Alemanha', 'Europa', 'Europa Ocidental', 'A Alemanha é conhecida por ter mais de 1.500 tipos diferentes de salsichas.'),
+(12, 'Índia', 'Ásia', 'Sul da Ásia', 'A Índia é o maior produtor de filmes do mundo, conhecida por Bollywood.'),
+(13, 'Rússia', 'Europa/Ásia', 'Europa Oriental e Norte da Ásia', 'A Rússia é o maior país do mundo em extensão territorial.'),
+(14, 'Argentina', 'América do Sul', 'Cone Sul', 'A Argentina é o país de origem do tango.'),
+(15, 'Itália', 'Europa', 'Europa Meridional', 'A Itália é o berço do Império Romano e da culinária como pizza e massas.');
 
 --
 -- Índices para tabelas despejadas
@@ -95,7 +102,7 @@ ALTER TABLE `cadcli`
 -- AUTO_INCREMENT de tabela `cadpais`
 --
 ALTER TABLE `cadpais`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
